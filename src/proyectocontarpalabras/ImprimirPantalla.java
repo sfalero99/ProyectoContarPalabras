6c9d0b383861a9ed3f90520eb3af5eb1053b5aac
Sergio <Sergio@PC-CASA>
2020-03-26 11:56:42 +0100

    Clase Imprimir Pantalla Modificada

--- src/proyectocontarpalabras/ImprimirPantalla.java
+++ src/proyectocontarpalabras/ImprimirPantalla.java
@@ -10,5 +10,24 @@ package proyectocontarpalabras;
  * @author Sergio
  */
 public class ImprimirPantalla {
+    String textoCaracteresTotal(int total){
+        return "El numero total de caracteres sin contar espacios es: " + total;
+    }
     
+    String textoNumeroPalabras(int total){
+        return "El numero total de palabras es: " + total;
+    }
+    
+    String textoTodosCaracteres(int[]total, char[] letra){
+        String resultado="";
+        for (int i = 0; i < total.length; i++) {
+            if (total[i]>1){
+                resultado += "El caracter " + letra[i] + " se repite " + total[i] + " veces.\n";
+            }else if(total[i]==1){
+                resultado += "El caracter " + letra[i] + " se repite " + total[i] + " vez.\n";
+            }
+        }    
+            return resultado;
+    } 
 }
+
