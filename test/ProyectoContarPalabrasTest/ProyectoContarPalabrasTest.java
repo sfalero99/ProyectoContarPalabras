ab6046ca55b1ba70d3fa86ab5d5846cc8acd47ed
Sergio <Sergio@PC-CASA>
2020-03-26 15:58:56 +0100

    Creacion clase ProyectoContarPalabrasTest

--- test/ProyectoContarPalabrasTest/ProyectoContarPalabrasTest.java
+++ test/ProyectoContarPalabrasTest/ProyectoContarPalabrasTest.java
@@ -36,10 +36,6 @@ public class ProyectoContarPalabrasTest {
     @After
     public void tearDown() {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
     // @Test
     // public void hello() {}
 }
